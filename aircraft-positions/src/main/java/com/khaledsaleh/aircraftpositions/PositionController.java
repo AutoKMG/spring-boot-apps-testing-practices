1ab48e22ecf222b94cf1b4925396fc3874cec860
Khaled Saleh <th.be03@gmail.com>
2023-10-24 22:51:51 +0200

    Annotating PositionController as RestController and updating .gitignore

--- aircraft-positions/src/main/java/com/khaledsaleh/aircraftpositions/PositionController.java
+++ aircraft-positions/src/main/java/com/khaledsaleh/aircraftpositions/PositionController.java
@@ -2,13 +2,13 @@ package com.khaledsaleh.aircraftpositions;
 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @RequiredArgsConstructor
-@Controller
+@RestController
 public class PositionController {
     @NonNull
     private final AircraftRepository repository;
