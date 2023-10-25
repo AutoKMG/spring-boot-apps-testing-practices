8f1e1563bd683cdfe5cfbca7d51b9667dde2d0a2
Khaled Saleh <th.be03@gmail.com>
2023-10-25 12:11:12 +0200

    Isolating PositionController behavior

--- aircraft-positions/src/main/java/com/khaledsaleh/aircraftpositions/PositionController.java
+++ aircraft-positions/src/main/java/com/khaledsaleh/aircraftpositions/PositionController.java
@@ -11,21 +11,10 @@ import org.springframework.web.reactive.function.client.WebClient;
 @RestController
 public class PositionController {
     @NonNull
-    private final AircraftRepository repository;
-    private WebClient client =
-            WebClient.create("http://localhost:7634/aircraft");
+    private final PositionRetriever retriever;
 
     @GetMapping("/aircraft")
-    public Iterable<Aircraft> getCurrentAircraftPositions(Model model) {
-        repository.deleteAll();
-
-        client.get()
-                .retrieve()
-                .bodyToFlux(Aircraft.class)
-                .filter(plane -> !plane.getReg().isEmpty())
-                .toStream()
-                .forEach(repository::save);
-
-        return repository.findAll();
+    public Iterable<Aircraft> getCurrentAircraftPositions() {
+        return retriever.retrieveAircraftPositions();
     }
 }
